045d3f5e04db529849c97d027d55a4a649994bce
Vaibhav Shinde <vaibhu8605@gmail.com>
2024-11-02 22:06:58 +0530

    Learn JS

--- Javascript/Date.js
+++ Javascript/Date.js
@@ -1,6 +1,6 @@
-let arr = [1,2,5,1,24,6,2,46,1,2]
+let arr = [1, 2, 5, 1, 24, 6, 2, 46, 1, 2]
 let newSet = new Set([])
-for(let i of arr){
+for (let i of arr) {
     newSet.add(i)
 }
 console.log(newSet.values())
