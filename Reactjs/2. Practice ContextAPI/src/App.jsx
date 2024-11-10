bf4f300248abbc2e4e2e9ca148120ce5edb67f04
Vaibhav Shinde <vaibhu8605@gmail.com>
2024-11-10 08:27:08 +0530

    ContextAPI

---
+++ Reactjs/2. Practice ContextAPI/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link, NavLink} from "react-router-dom"
+
+const App = () => {
+  return (
+    <div className='text-[red]'>App</div>
+  )
+}
+
+export default App
