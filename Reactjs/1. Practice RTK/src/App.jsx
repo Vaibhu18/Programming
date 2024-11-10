69d36741fe417a2a9f56727394bb208f111e7af2
Vaibhav Shinde <vaibhu8605@gmail.com>
2024-11-10 18:35:50 +0530

    RTK

--- Reactjs/1. Practice RTK/src/App.jsx
+++ Reactjs/1. Practice RTK/src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import {Link, NavLink} from "react-router-dom"
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./app/features/counter/counterSlice";
 
 const App = () => {
+  const count = useSelector((state) => state.vaibhav.value);
+  const dispatch = useDispatch();
   return (
-    <div className='text-[red]'>App</div>
-  )
-}
+    <>
+      <h1>{count}</h1>
+      <button
+        onClick={() => dispatch(increment())}
+        className="border border-black px-2 py-1 mr-3 mt-5"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="border border-black px-2 py-1 "
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
 
-export default App
+export default App;
